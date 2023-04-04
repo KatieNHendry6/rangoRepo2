a7632513f30c81259f0c6d76222453f414f8771e
KatieNHendry6 <2830068h@student.gla.ac.uk>
2023-04-04 11:55:14 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -24,7 +24,9 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 
 def show_category(request, category_name_slug):
@@ -73,7 +75,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
